ef6b854dab2bdcd1e60e66a666bde10701f5dd38
Merchuli <merchu.liang@gmail.com>
2022-01-21 16:03:47 +0800

    Add index.html and index.js compiled from index.ts

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const test = '123';
+console.log('Hello World of my variable test: ', test);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 const test:string = '123';
+console.log('Hello World of my variable test: ', test);
