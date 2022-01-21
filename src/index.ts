bb41661edd53f905e23ace7be3453345314f9d96
Merchuli <merchu.liang@gmail.com>
2022-01-21 16:41:55 +0800

    Add samples of type array and tuple

--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,21 @@ whatEver = true;
 whatEver = 123;
 whatEver = 'whatEver';
 
+/** 
+ * ==== Object Type ===
+ **/
+
+/** Array, 陣列 */
+const arr: string[] = ['a', 'b'];
+const arrx2: number[][] = [[1, 2, 3], [4, 5 , 6]];
+
+/**
+ * 陣列的延伸： Tuple, 元祖
+ * 嚴格的陣列，陣列元素數量是固定的，且每個元素型別都是已知的，但個別型別不用相同。
+ * The definition of "Tuple" from Cambridge Dictionary:
+ * "a structure of data that has several parts"
+ * */
+const tuple: [number, string, boolean] = [1, 'a', true];
+const tuple2: [string, string][] = [['a', 'b'], ['c', 'd']];
+
 
